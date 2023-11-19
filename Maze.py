0c8e79b24368ce7122249944420f3060cf2ec9ae
BuiChienThang1 <thangdev.hcmute@gmail.com>
2023-11-19 20:41:00 +0700

    Nearly Done

--- Maze.py
+++ Maze.py
@@ -12,10 +12,10 @@ from queue import Queue
 import heapq
 
 
-
-#colors
+# colors
 one = (79, 189, 186)
-two = (206, 171, 147)
+# two = (206, 171, 147)
+two = (255, 251, 233)
 three = (227, 202, 165)
 four = (255, 251, 233)
 five = (246, 137, 137)
@@ -24,14 +24,14 @@ seven = (0, 255, 0)
 
 pygame.init()
 
-size = (706, 706)
+size = (706 - 66, 706 - 66)
 screen = pygame.display.set_mode(size)
 
-pygame.display.set_caption("MAZE")
+pygame.display.set_caption("DOT GAME")
 
 width = 20
 height = 20
-margin = 2
+margin = 0
 
 grid = [[0 for x in range(33)] for y in range(33)]
 gobalStartPoint = None
@@ -40,26 +40,35 @@ gobalEndPoint = None
 done = False
 clock = pygame.time.Clock()
 found = False
-neighbour=[]
+neighbour = []
+
+algorithm = ""
+
+
+algorithms = ["bfs", "dfs", "a_star", "dijkstra"]
+algorithm_index = 0
+
 
 def savegrid():
     global grid
-    
-    np.savetxt(r"./maze.txt",grid)
+
+    np.savetxt(r"./maze.txt", grid)
+
+
 def loadgrid(index):
     global grid
-    if(index ==0):
+    if index == 0:
         grid = np.loadtxt(r"./maze.txt").tolist()
-    elif(index ==1):
-        grid = np.loadtxt(r'./Maze1/maze.txt').tolist()
-    elif(index ==2):
-        grid = np.loadtxt(r'./Maze2/maze.txt').tolist()
-    elif(index ==3):
-        grid = np.loadtxt(r'./Maze3/maze.txt').tolist()
-    elif(index ==4):
-        grid = np.loadtxt(r'./Maze4/maze.txt').tolist()
-    elif(index ==5):
-        grid = np.loadtxt(r'./Maze5/maze.txt').tolist()
+    elif index == 1:
+        grid = np.loadtxt(r"./Maze1/maze.txt").tolist()
+    elif index == 2:
+        grid = np.loadtxt(r"./Maze2/maze.txt").tolist()
+    elif index == 3:
+        grid = np.loadtxt(r"./Maze3/maze.txt").tolist()
+    elif index == 4:
+        grid = np.loadtxt(r"./Maze4/maze.txt").tolist()
+    elif index == 5:
+        grid = np.loadtxt(r"./Maze5/maze.txt").tolist()
 
     # elif(index ==3):
     #     grid = np.loadtxt(r'./Downloads/Maze-Pathfinding-main/Maze3/maze.txt').tolist()
@@ -67,10 +76,11 @@ def loadgrid(index):
     #     grid = np.loadtxt(r'./Downloads/Maze-Pathfinding-main/Maze4/maze.txt').tolist()
     # elif(index ==5):
     #     grid = np.loadtxt(r'./Downloads/Maze-Pathfinding-main/Maze5/maze.txt').tolist()
-        
+
+
 def bfs_shortestpath(maze, path=""):
     global grid
-    i,j=startp(maze,0,0)
+    i, j = startp(maze, 0, 0)
     pos = set()
     for move in path:
         if move == "L":
@@ -85,22 +95,23 @@ def bfs_shortestpath(maze, path=""):
         elif move == "D":
             j += 1
         pos.add((j, i))
-    
+
     for j, row in enumerate(maze):
         for i, col in enumerate(row):
             if (j, i) in pos:
-                
                 grid[j][i] = 4
-                
-def startp(maze,i,j):
+
+
+def startp(maze, i, j):
     for x in range(len(maze[0])):
         try:
-            i =(maze[x].index(2))
+            i = maze[x].index(2)
             j = x
             print(j)
-            return i,j
+            return i, j
         except:
             pass
+
 
 # def bfs(maze, moves,i,j):
 #     global found
@@ -136,7 +147,7 @@ def startp(maze,i,j):
 #     nums.put("")
 #     add = ""
 #     i,ii =startp(grid,0,0)
-#     while found != True: 
+#     while found != True:
 #         add = nums.get()
 #         for j in ["L", "R", "U", "D"]:
 #             put = add + j
@@ -144,6 +155,7 @@ def startp(maze,i,j):
 #                 nums.put(put)
 #             if(found == True):
 #                 break
+
 
 def bfs():
     global grid, neighbour
@@ -162,9 +174,17 @@ def bfs():
         if current == end:
             print("Finishing")
             short_path(came_from, end)
+
             return True
 
-        visited.add(current)
+        if current not in visited:
+            visited.add(current)
+            # Simulation code to visualize the BFS process
+            grid[current[0]][current[1]] = 5
+            print_grid(grid)  # Function to display the grid (customize as needed)
+            time.sleep(0.05)  # Add a delay to make it slower
+
+        # visited.add(current)
 
         for nei in neighbour[current[0] * len(grid[0]) + current[1]]:
             if nei not in visited:
@@ -172,7 +192,6 @@ def bfs():
                 came_from[nei] = current
 
     return False
-
 
 
 # def dfs():
@@ -203,7 +222,6 @@ def bfs():
 #     return False
 
 
-
 def dfs():
     global grid, neighbour
     neighbourr()
@@ -229,7 +247,11 @@ def dfs():
             print_grid(grid)  # Function to display the grid (customize as needed)
             time.sleep(0.05)  # Add a delay to make it slower
 
-        unvisited_neighbors = [nei for nei in neighbour[current[0] * len(grid[0]) + current[1]] if nei not in visited]
+        unvisited_neighbors = [
+            nei
+            for nei in neighbour[current[0] * len(grid[0]) + current[1]]
+            if nei not in visited
+        ]
 
         if unvisited_neighbors:
             next_neighbor = unvisited_neighbors[0]
@@ -238,7 +260,7 @@ def dfs():
         else:
             # No unvisited neighbors, backtrack
             stack.pop()
-    
+
     return False
 
 
@@ -278,6 +300,7 @@ def dijkstra():
                 came_from[nei] = current
 
     return False
+
 
 def simulate_dijkstra_process(came_from, start, end, visited):
     # Backtrack to find the solution path
@@ -326,74 +349,78 @@ def print_grid(grid):
             else:
                 color = four
 
-            pygame.draw.rect(screen, color, [margin + (margin + width) * column, margin + (margin + height) * row, width, height])
+            pygame.draw.rect(
+                screen,
+                color,
+                [
+                    margin + (margin + width) * column,
+                    margin + (margin + height) * row,
+                    width,
+                    height,
+                ],
+            )
 
     pygame.display.flip()
 
 
-
-
-            
 def neighbourr():
-    global grid,neighbour
-    neighbour = [[]for col in range(len(grid)) for row in range(len(grid))]
-    count=0
+    global grid, neighbour
+    neighbour = [[] for col in range(len(grid)) for row in range(len(grid))]
+    count = 0
     for i in range(len(grid)):
         for j in range(len(grid)):
             neighbour[count] == []
-            if (i > 0 and grid[i - 1][j] != 1):
-                neighbour[count].append((i-1,j))
-            if (j > 0 and grid[i][j - 1] != 1):
-                neighbour[count].append((i,j-1))
-            if (i < len(grid) - 1 and grid[i + 1][j] != 1):
-                neighbour[count].append((i+1,j))
-            if (j < len(grid) - 1 and grid[i][j + 1] != 1):
-                neighbour[count].append((i,j+1))
-            count+=1
-            
-            
-    
-            
-def h(p1, p2):
-	x1, y1 = p1
-	x2, y2 = p2
-	return abs(x1 - x2) + abs(y1 - y2)
+            if i > 0 and grid[i - 1][j] != 1:
+                neighbour[count].append((i - 1, j))
+            if j > 0 and grid[i][j - 1] != 1:
+                neighbour[count].append((i, j - 1))
+            if i < len(grid) - 1 and grid[i + 1][j] != 1:
+                neighbour[count].append((i + 1, j))
+            if j < len(grid) - 1 and grid[i][j + 1] != 1:
+                neighbour[count].append((i, j + 1))
+            count += 1
 
-def S_E(maze,start,end):
+
+def h(p1, p2):
+    x1, y1 = p1
+    x2, y2 = p2
+    return abs(x1 - x2) + abs(y1 - y2)
+
+
+def S_E(maze, start, end):
     for x in range(len(grid)):
         for y in range(len(grid[x])):
-            if(grid[x][y]==2):
-                start =x,y
-            if(grid[x][y]==3):
-                end =x,y
-       
-    return start,end
+            if grid[x][y] == 2:
+                start = x, y
+            if grid[x][y] == 3:
+                end = x, y
+
+    return start, end
+
 
 def short_path(came_from, current):
-     grid[current[0]][current[1]] = 4
-     while current in came_from:
-         current = came_from[current]
-         grid[current[0]][current[1]] = 4
-        
-    
-        
+    grid[current[0]][current[1]] = 4
+    while current in came_from:
+        current = came_from[current]
+        grid[current[0]][current[1]] = 4
+
+
 def a_star():
     global grid, neighbour
     neighbourr()
 
-    start,end = S_E(grid,0,0)
+    start, end = S_E(grid, 0, 0)
     count = 0
     open_set = PriorityQueue()
     open_set.put((0, count, start))
     open_set_his = {start}
     came_from = {}
-    
-    g_score = [float("inf") for row in grid for spot in row ]
-    g_score[start[0]*len(grid[0]) +start[1]] = 0
-    f_score = [ float("inf") for row in grid for spot in row ]
-    f_score[start[0]*len(grid[0]) +start[1]] = h(start, end)
 
-    
+    g_score = [float("inf") for row in grid for spot in row]
+    g_score[start[0] * len(grid[0]) + start[1]] = 0
+    f_score = [float("inf") for row in grid for spot in row]
+    f_score[start[0] * len(grid[0]) + start[1]] = h(start, end)
+
     while not open_set.empty():
         current = open_set.get()[2]
         open_set_his.remove(current)
@@ -401,30 +428,33 @@ def a_star():
             print("finishing")
             short_path(came_from, end)
             return True
-        for nei in neighbour[current[0]*len(grid[0]) +current[1]]:
-            temp_g_score = g_score[current[0]*len(grid[0]) +current[1]] + 1
-            if temp_g_score < g_score[nei[0]*len(grid[0]) +nei[1]]:
+
+        for nei in neighbour[current[0] * len(grid[0]) + current[1]]:
+            temp_g_score = g_score[current[0] * len(grid[0]) + current[1]] + 1
+            if temp_g_score < g_score[nei[0] * len(grid[0]) + nei[1]]:
                 came_from[nei] = current
-                g_score[nei[0]*len(grid[0]) +nei[1]] = temp_g_score
-                f_score[nei[0]*len(grid[0]) +nei[1]] = temp_g_score + h(nei, end)
+                g_score[nei[0] * len(grid[0]) + nei[1]] = temp_g_score
+                f_score[nei[0] * len(grid[0]) + nei[1]] = temp_g_score + h(nei, end)
                 if nei not in open_set_his:
                     count += 1
-                    open_set.put((f_score[nei[0]*len(grid[0]) +nei[1]], count, nei))
+                    open_set.put((f_score[nei[0] * len(grid[0]) + nei[1]], count, nei))
                     open_set_his.add(nei)
+
+                    grid[current[0]][current[1]] = 5
+                    print_grid(
+                        grid
+                    )  # Function to display the grid (customize as needed)
+                    time.sleep(0.05)  # Add a delay to make it slower
                     # grid[nei[0]][nei[1]] = 5
                     # pygame.display.update()
                     # time.sleep(0.01)
-    
+
         # if current != start:
         #     grid[current[0]][current[1]] = 6
         #     pygame.display.update()
         #     time.sleep(0.01)
-        
 
     return False
-
-
-
 
 
 # def generate_solvability_maze_with_user_points(start_x, start_y, end_x, end_y):
@@ -466,7 +496,7 @@ def a_star():
 #     grid[end_x][end_y] = 3
 
 
-    # Ensure that the maze is solvable (you can add a solvability check here)
+# Ensure that the maze is solvable (you can add a solvability check here)
 
 # ...
 
@@ -480,7 +510,6 @@ def generate_solvability_maze_with_user_points(start_x, start_y, end_x, end_y):
     def get_neighbours(x, y):
         neighbours = []
         for dx, dy in [(2, 0), (-2, 0), (0, 2), (0, -2)]:
-        
             new_x, new_y = x + dx, y + dy
             if is_valid(new_x, new_y):
                 neighbours.append((new_x, new_y))
@@ -506,8 +535,6 @@ def generate_solvability_maze_with_user_points(start_x, start_y, end_x, end_y):
     # Generate the maze
     create_maze(start_x, start_y)
 
-    
-
     # Ensure the maze is solvable by connecting all visited cells
     # for y in range(1, 32, 2):
     #     for x in range(1, 32, 2):
@@ -529,113 +556,80 @@ def generate_solvability_maze_with_user_points(start_x, start_y, end_x, end_y):
     return grid
 
 
-
-
-
-
-      
-
-
-
-
-
-
-
 while not done:
-    gobalStartPoint 
-    gobalEndPoint 
+    gobalStartPoint
+    gobalEndPoint
 
-    for event in pygame.event.get(): 
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
         elif event.type == pygame.KEYDOWN:
-             if event.key == pygame.K_ESCAPE:
-                    print("Exit")
-                    pygame.quit()
-             if event.key == pygame.K_s:
-                 print("Saving Maze")
-                 savegrid()
-             if event.key == pygame.K_l:
-                 print("Loading Maze")
-                 loadgrid(0)
-             if event.key == pygame.K_f:
-                 print("Filling Maze")
-                 grid = [[1 for x in range(33)] for y in range(33)]
-             if event.key == pygame.K_1:
-                 print("Loading Maze 1")
-                 loadgrid(1)
-             if event.key == pygame.K_2:
-                 print("Loading Maze 2")
-                 loadgrid(2)
-             if event.key == pygame.K_3:
-                 print("Loading Maze 3")
-                 loadgrid(3)
-             if event.key == pygame.K_4:
-                 print("Loading Maze 4")
-                 loadgrid(4)
-             if event.key == pygame.K_5:
-                 print("Loading Maze 5")
-                 loadgrid(5)
-             if event.key == pygame.K_RETURN:
-                if((sum(x.count(2) for x in grid)) == 1):
+            if event.key == pygame.K_e:
+                print("Refresh ")
+                loadgrid(0)
+
+            if event.key == pygame.K_RETURN:
+                savegrid()  # Save the generated maze
+                if (sum(x.count(2) for x in grid)) == 1:
                     print("Solving")
-                    #bfs()
-                    #a_star()
-                    #dfs()
-                    #dfs_simulation()
-                    dijkstra()
-             if event.key == pygame.K_q:
-                print("Creating and loading a random maze")
-                try:
-                    #generate_random_maze()
-                    #generate_random_maze_with_user_points(0, 1, 20, 20)
-                    generate_solvability_maze_with_user_points(gobalStartPoint[0], gobalStartPoint[1], gobalEndPoint[0], gobalEndPoint[1])
-
-                    savegrid()  # Save the generated maze
-                    np.savetxt(r"./maze.txt",grid) # Load the generated maze
-                except:
-                    print("Please choose start and end point")
-
-
-             if event.key == pygame.K_r:
-
+                    if algorithm == "dijkstra":
+                        dijkstra()
+                    elif algorithm == "bfs":
+                        bfs()
+                    elif algorithm == "dfs":
+                        dfs()
+                    elif algorithm == "a_star":
+                        a_star()
+                    # bfs()
+                    # a_star()
+                    # dfs()
+                    # dfs_simulation()
+                    # dijkstra()
+            if event.key == pygame.K_r:
                 grid = [[0 for x in range(33)] for y in range(33)]
+
+            if event.key == pygame.K_w:
+                algorithm_index = (algorithm_index + 1) % len(algorithms)
+                algorithm = algorithms[algorithm_index]
+                print(f"Current Algorithm: {algorithm}")
+
         if pygame.mouse.get_pressed()[2]:
             column = pos[0] // (width + margin)
             row = pos[1] // (height + margin)
-            if((sum(x.count(2) for x in grid)) < 1 or (sum(x.count(3) for x in grid)) < 1):
-                if((sum(x.count(2) for x in grid)) == 0):
-                    if(grid[row][column] == 2):
+            if (sum(x.count(2) for x in grid)) < 1 or (
+                sum(x.count(3) for x in grid)
+            ) < 1:
+                if (sum(x.count(2) for x in grid)) == 0:
+                    if grid[row][column] == 2:
                         grid[row][column] = 0
-                    elif(grid[row][column] == 3):
+                    elif grid[row][column] == 3:
                         grid[row][column] = 0
                     else:
-                        grid[row][column]  = 2
+                        grid[row][column] = 2
                         gobalStartPoint = (row, column)
                 else:
-                    if(grid[row][column] == 3):
+                    if grid[row][column] == 3:
                         grid[row][column] = 0
-                    elif(grid[row][column] == 2):
+                    elif grid[row][column] == 2:
                         grid[row][column] = 0
                     else:
-                        grid[row][column]  = 3
+                        grid[row][column] = 3
                         gobalEndPoint = (row, column)
 
             else:
-                if(grid[row][column] == 2):
+                if grid[row][column] == 2:
                     grid[row][column] = 0
-                if(grid[row][column] == 3):
+                if grid[row][column] == 3:
                     grid[row][column] = 0
-                if(grid[row][column] == 1):
+                if grid[row][column] == 1:
                     grid[row][column] = 0
-        if pygame.mouse.get_pressed()[0]:
-            # if(event.button == 1):
-            column = pos[0] // (width + margin)
-            row = pos[1] // (height + margin)
-            print("left click")
-            grid[row][column] = 1
-        
-                
+        # if pygame.mouse.get_pressed()[0]:
+        #     # if(event.button == 1):
+        #     column = pos[0] // (width + margin)
+        #     row = pos[1] // (height + margin)
+        #     print("left click")
+        #     grid[row][column] = 1
+
     pos = pygame.mouse.get_pos()
     x = pos[0]
     y = pos[1]
@@ -656,11 +650,16 @@ while not done:
                 color = seven
             else:
                 color = four
-            pygame.draw.rect(screen, color, [margin + (margin + width) * column, margin + (margin + height) * row, width, height])
+            pygame.draw.rect(
+                screen,
+                color,
+                [
+                    margin + (margin + width) * column,
+                    margin + (margin + height) * row,
+                    width,
+                    height,
+                ],
+            )
     pygame.display.flip()
     clock.tick(60)
 pygame.quit()
-
-
-
-
